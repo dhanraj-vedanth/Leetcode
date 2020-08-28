985497bde1bdaf96e9b08e8b22d3def57851fcf8
Dhanraj Raghunathan <draghun@ncsu.edu>
2020-08-28 00:59:41 -0400

    voluntary test fail

--- tests/test_buy_sell_stock.py
+++ tests/test_buy_sell_stock.py
@@ -3,5 +3,5 @@ from buy_sell_stock_121.main import Solution
 
 def test_working_case():
     s = Solution()
-    assert (s.maxProfit([7, 1, 5, 3, 6, 4]) == 5)
+    assert (s.maxProfit([7, 1, 5, 3, 6, 4]) == 4)
     assert (s.maxProfit([7, 6, 4, 3, 1]) == 0)
